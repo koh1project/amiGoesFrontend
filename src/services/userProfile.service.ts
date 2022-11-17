3259b6e07ab582d5d1b96176ad38808f70d9a2c8
Roberto Guevara <robertoeguevara@gmail.com>
2022-11-16 17:45:04 -0800

    feat: profileScreen - add getUserProfile function

--- src/services/userProfile.service.ts
+++ src/services/userProfile.service.ts
@@ -1,9 +1,11 @@
-//import { post } from './api';
-import { auth } from '../utils/firebase';
+import { get } from './api';
+import { useAuthContext } from '../components/auth/AuthContextProvider';
+// import { auth } from '../utils/firebase';
 
-export const authUser = () => {
-  return auth.currentUser;
-};
+// export const authUser = () => {
+//   return auth.currentUser;
+// };
+const user = useAuthContext();
 
 export const CREATE_USERPROFILE_ENDPOINT = {
   post: '/amigos/create',
@@ -15,4 +17,17 @@ export const UPDATE_USERPROFILE_ENDPOINT = {
 
 export const UPDATE_NOTIFICATION_TOKEN_ENDPOINT = {
   patch: '/notifications/addNotificationToken/',
+};
+
+const GET_USERPROFILE_ENDPOINT = {
+  get: '/amigos/',
+};
+
+export const getUserProfile = async () => {
+  try {
+    return get((GET_USERPROFILE_ENDPOINT + user.id).get);
+  } catch (error) {
+    console.error('API getUserProfile ERROR', error);
+    throw error;
+  }
 };
